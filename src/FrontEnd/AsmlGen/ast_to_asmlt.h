90b4f5d56e7057ecdffcd740cfdc0121c43fd944
max <maxime.raynal@gmail.com>
2017-12-21 16:47:18 +0100

    ajout fichiers qui linkent le front end au back end, non testés, même pas compilés, en attendant d'être intégrés

---
+++ src/FrontEnd/AsmlGen/ast_to_asmlt.h
@@ -0,0 +1,13 @@
+#ifndef AST_TO_ASMLT_H
+#define AST_TO_ASMLT_H
+
+#include "asml_parser_types.h"
+#include "ast.h"
+
+asml_asmt_t ast_to_asmlt(ptree t);
+
+asml_exp_t *to_asml_exp(ptree t);
+
+asml_formal_arg_t *args_list_to_asml_args_list(plist ast_args_list)
+
+#endif

35c4ac1729ceae738f8d02f9143e1d0e6647a576
berurier <beruriervon77@gmail.com>
2018-01-15 13:52:24 +0100

    ajout commentaire

--- src/BackEnd/parser_asml/src/arm_funcall.h
+++ src/BackEnd/parser_asml/src/arm_funcall.h
@@ -7,23 +7,52 @@
 #define __ARM_FUNCALL_H__
 
 #include "arm_instruction.h"
-
+/**
+ * \file      arm_funcall.h
+ * \author    The C Team - Enzo Brignon
+ * \version   1.0
+ * \date      17 Janvier 2018
+ * \brief     Function calling implementation in ARM
+ *
+ */
 namespace arm {
-
+/*! \class arm_function
+ * \brief function call Object
+ */
   class arm_funcall : public arm_instruction {
   public:
+    /*!
+     *  \brief Constructor of the class arm_funcall
+     *
+     */
     arm_funcall (void);
     virtual string get_instruction (void);
     virtual void set_function_name (string);
     virtual void set_retval (string);
     virtual string get_retval (void);
+    /*!
+     *  \brief Number of Register
+     *
+     *  Return the number of registers
+     *  \return int of register
+     */
     virtual int nb_regs (void);
+    /*!
+     * \brief Getter of operands
+     * Return the list of operands
+     * \return list of string of operands
+     */
     virtual list<string>* get_op_list (void);
+    /*!
+     * \brief TODO
+     * \param instr string of instruction name
+     * \return boolean
+     */
     static bool immediate (string);
   protected:
-    string function_name;
-    bool is_returning;
-    string retval;
+    string function_name; /*!< function name              */
+    bool is_returning; /*!< boolean to check if function return something              */
+    string retval; /*!< return value name             */
   };
 
 }
--- src/BackEnd/parser_asml/src/arm_function.h
+++ src/BackEnd/parser_asml/src/arm_function.h
@@ -7,22 +7,46 @@
 #define __ARM_FUNCTION_H__
 
 #include "arm_function_generator.h"
-
+/**
+ * \file      arm_function.h
+ * \author    The C Team - Enzo Brignon
+ * \version   1.0
+ * \date      17 Janvier 2018
+ * \brief     Function management implementation in ARM
+ *
+ */
 using namespace std;
 
 namespace arm {
-
+/*! \class arm_function
+ * \brief function Object
+ */
   class arm_function {
     friend class arm_function_generator;
   public:
+    /*!
+     *  \brief TODO
+     *
+     */
     static void initialize (void);
     static void set_processed_function (string);
+    /*!
+     *  \brief TODO
+     *  \return string
+     *
+     */
     static string process_refs (void);
+    /*!
+     *  \brief TODO
+     *  \param string name
+     *  \return string 
+     *
+     */
     static string add_ref (string);
   protected:
-    static list<string>* funcnames_list;
-    static string processed_function;
-    static map<string, string>* ref_map;
+    static list<string>* funcnames_list; /*!< list of function names              */
+    static string processed_function; /*!< processed function             */
+    static map<string, string>* ref_map; /*!< map of references             */
   };
 
 }

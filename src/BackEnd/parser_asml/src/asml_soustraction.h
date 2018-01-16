63f5f4996bcacd4997e01aa9baf7c02334e5b8b9
berurier <beruriervon77@gmail.com>
2018-01-16 17:43:45 +0100

    ajout com header

--- src/BackEnd/parser_asml/src/asml_soustraction.h
+++ src/BackEnd/parser_asml/src/asml_soustraction.h
@@ -19,7 +19,10 @@ using namespace std;
  *  \brief     Contains the classes that manages the asml
  */
 namespace asml {
-
+  /*! \class   asml_soustraction
+   *  \brief   class that represent an addition in asml
+   *  \details it inherits from asml_instruction
+   */
   class asml_soustraction : public asml_instruction {
   public:
     asml_soustraction (void);
@@ -32,14 +35,20 @@ namespace asml {
     virtual void set_op1 (string);
     virtual void set_op2 (string);
     virtual void set_op3 (string);
-
+    /**
+     * \brief    This function generates the ASML code that correspond to this instruction
+     * \details  This function is used to generate ASML code.
+     * \param    ident an int that is the number of tabulations to insert to indent
+     *           correctly the generated code
+     * \return   a string containing the generated code
+     */
     virtual string generate (int);
   protected:
-    string op1;
-    string op2;
-    string op3;
+    string op1; /*! The first operand of the substraction in let op1 = sub op2 op3 in .. */
+    string op2; /*! The second operand of the substraction in let op1 = sub op2 op3 in .. */
+    string op3; /*! The third operand of the substraction in let op1 = sub op2 op3 in .. */
   };
 
-} // TODO
+} 
 
 #endif //__ASML_SOUSTRACTION_H__
